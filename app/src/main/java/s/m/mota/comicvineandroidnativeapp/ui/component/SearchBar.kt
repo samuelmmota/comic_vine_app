c13d7cf3f41df07cf1b523f086a68cc200c2df0f
Samuel Mota <samuelmmota@gmail.com>
2024-12-04 18:18:51 +0000

    Add strings xml, Theme.kt

---
+++ app/src/main/java/s/m/mota/comicvineandroidnativeapp/ui/component/SearchBar.kt
@@ -0,0 +1,83 @@
+package s.m.mota.comicvineandroidnativeapp.ui.component
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import s.m.mota.comicvineandroidnativeapp.ui.screens.mainscreen.MainViewModel
+import s.m.mota.comicvineandroidnativeapp.ui.theme.Blue
+
+@OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
+@Composable
+fun SearchBar(isAppBarVisible: MutableState<Boolean>, viewModel: MainViewModel) {
+    var text by remember { mutableStateOf("") }
+    val focusRequester = FocusRequester()
+    BackHandler(isAppBarVisible.value.not()) {
+        isAppBarVisible.value = true
+    }
+    Column {
+        TextField(modifier = Modifier
+            .fillMaxWidth()
+            .focusRequester(focusRequester),
+            value = text,
+            colors = TextFieldDefaults.colors(
+                cursorColor = Color.Black,
+                disabledLabelColor = Blue,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            onValueChange = {
+                text = it
+                viewModel.search(it)
+            },
+            shape = RoundedCornerShape(8.dp),
+            singleLine = true,
+            trailingIcon = {
+                if (text.trim().isNotEmpty()) {
+                    Icon(Icons.Filled.Clear,
+                        contentDescription = "clear text",
+                        modifier = Modifier
+                            .padding(end = 16.dp)
+                            .offset(x = 10.dp)
+                            .clickable {
+                                text = ""
+                            })
+                } else {
+                    Icon(Icons.Filled.Search,
+                        contentDescription = "search",
+                        modifier = Modifier
+                            .padding(end = 16.dp)
+                            .offset(x = 10.dp)
+                            .clickable {
+
+                            })
+                }
+            })
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
+        }
+    }
+}
--- app/src/main/java/s/m/mota/comicvineandroidnativeapp/ui/theme/Theme.kt
+++ app/src/main/java/s/m/mota/comicvineandroidnativeapp/ui/theme/Theme.kt
@@ -1,26 +1,22 @@
 package s.m.mota.comicvineandroidnativeapp.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val DarkColorPalette = darkColorScheme(
+    primary = Purple200,
+    primaryContainer = Purple700,
+    secondary = Teal200
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+private val LightColorPalette = lightColorScheme(
+    primaryContainer = Purple500,
+    primary = Purple700,
+    secondary = Teal200,
+    tertiary = Blue,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -36,23 +32,18 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun ComicVineAndroidNativeAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable() () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
