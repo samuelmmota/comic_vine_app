ae4dc4ee1f49ac963817a82709a781f13f51c4af
Samuel Mota <samuelmmota@gmail.com>
2024-12-05 16:05:23 +0000

    Changed AppConstants value for shimmer efect when loading images

--- app/src/main/java/s/m/mota/comicvineandroidnativeapp/utils/AppConstant.kt
+++ app/src/main/java/s/m/mota/comicvineandroidnativeapp/utils/AppConstant.kt
@@ -4,4 +4,4 @@ object AppConstant {
     const val MINIMIZED_MAX_LINES = 2
 }
 const val ACTIVE_CHARACTERS_TAB = 0
-const val CircularRevealPluginDuration = 200
+const val CircularRevealPluginDuration = 50
