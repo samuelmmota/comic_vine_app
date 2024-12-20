8d0f96b01357b44e7f2061d4e377dee6c9972fab
Samuel Mota <samuelmmota@gmail.com>
2024-12-20 17:47:27 +0000

    Added Volumes API methods

--- app/src/main/java/s/m/mota/comicvineandroidnativeapp/data/datasource/remote/ApiService.kt
+++ app/src/main/java/s/m/mota/comicvineandroidnativeapp/data/datasource/remote/ApiService.kt
@@ -6,6 +6,7 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 import s.m.mota.comicvineandroidnativeapp.BuildConfig
 import s.m.mota.comicvineandroidnativeapp.data.model.ComicIssue
+import s.m.mota.comicvineandroidnativeapp.data.model.ComicVolume
 import s.m.mota.comicvineandroidnativeapp.data.model.Formats
 import s.m.mota.comicvineandroidnativeapp.data.model.character.ComicCharacter
 import s.m.mota.comicvineandroidnativeapp.data.model.response.ComicVineApiResponse
@@ -77,4 +78,30 @@ interface ApiService {
         @Query("format") format: String = Formats.JSON.name,
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
     ): ComicVineApiResponse<ComicIssue>
+
+    @Headers("User-Agent: ComicVineClient/1.0")
+    @GET("volumes/")
+    suspend fun getVolumes(
+        @Query("field_list") filedList: String? = null,
+        @Query("limit") limit: Int? = null,
+        @Query("offset") offset: Int? = null,
+        @Query("sort") sort: String? = null,// EX: &sort=field:direction where direction is either asc or desc
+        @Query("filter") filter: String? = null,
+        /*The result can be filtered by the marked fields in the Fields section below.
+          Single filter: &filter=field:value
+          Multiple filters: &filter=field:value,field:value
+          Date filters: &filter=field:start value|end value (using datetime format)
+    */
+        @Query("format") format: String = Formats.JSON.name,
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+    ): ComicVineApiResponse<List<ComicVolume>>
+
+    @Headers("User-Agent: ComicVineClient/1.0")
+    @GET("volume/{volumeApiId}/")
+    suspend fun getVolumeDetails(
+        @Path("volumeApiId") volumeId: String,
+        @Query("field_list") filedList: String? = null,
+        @Query("format") format: String = Formats.JSON.name,
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+    ): ComicVineApiResponse<ComicVolume>
 }
