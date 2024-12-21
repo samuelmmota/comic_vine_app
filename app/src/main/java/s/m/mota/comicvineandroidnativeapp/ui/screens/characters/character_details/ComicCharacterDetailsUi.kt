e8eee3c5e6b5672df50c9a0ed95b6a21d4488c00
Samuel Mota <samuelmmota@gmail.com>
2024-12-21 12:03:55 +0000

    Updated Comic UI Models to support nullable values

--- app/src/main/java/s/m/mota/comicvineandroidnativeapp/ui/screens/characters/character_details/ComicCharacterDetailsUi.kt
+++ app/src/main/java/s/m/mota/comicvineandroidnativeapp/ui/screens/characters/character_details/ComicCharacterDetailsUi.kt
@@ -1,13 +1,13 @@
 package s.m.mota.comicvineandroidnativeapp.ui.screens.characters.character_details
 
 data class ComicCharacterDetailsUi(
-    val id: String,
-    val name: String,
-    val realName: String,
+    val id: String?,
+    val name: String?,
+    val realName: String?,
     val imageUrl: String,
-    val aliases: List<String>,
-    val creators: List<String>,
-    val powers: List<String>,
-    val firstAppearedInIssue: String,
-    val description: String,
+    val aliases: List<String>?,
+    val creators: List<String>?,
+    val powers: List<String>?,
+    val firstAppearedInIssue: String?,
+    val description: String?,
 )
--- app/src/main/java/s/m/mota/comicvineandroidnativeapp/ui/screens/volumes/ComicVolumeUi.kt
+++ app/src/main/java/s/m/mota/comicvineandroidnativeapp/ui/screens/volumes/ComicVolumeUi.kt
@@ -1,17 +1,13 @@
 package s.m.mota.comicvineandroidnativeapp.ui.screens.volumes
 
 data class ComicVolumeUi(
-    val id: String,
-    val name: String,
+    val id: String?,
+    val name: String?,
     val volumeApiId: String?,
     val imageUrl: String,
-    val countOfIssues: String,
-    val dateAdded: String,
-    val dateLastUpdated: String,
-    val firstIssueNumber: String,
-    val firstIssueName: String,
-    val lastIssueNumber: String,
-    val lastIssueName: String,
-    val publisher: String,
-    val startYear: String,
+    val countOfIssues: String?,
+    val dateLastUpdated: String?,
+    val lastIssueName: String?,
+    val publisher: String?,
+    val startYear: String?,
 )
