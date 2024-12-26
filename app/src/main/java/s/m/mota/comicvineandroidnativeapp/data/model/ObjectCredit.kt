3cb4aa03ccb2c7e88f8cce341451f81c40d23a98
Samuel Mota <samuelmmota@gmail.com>
2024-12-26 22:42:33 +0000

    Added data model StoryArcCredits and make Team,Object,Concept and Person Credit data models implement ComicResource interface

--- app/src/main/java/s/m/mota/comicvineandroidnativeapp/data/model/ObjectCredit.kt
+++ app/src/main/java/s/m/mota/comicvineandroidnativeapp/data/model/ObjectCredit.kt
@@ -1,8 +1,15 @@
 package s.m.mota.comicvineandroidnativeapp.data.model
 
 import com.google.gson.annotations.SerializedName
+import s.m.mota.comicvineandroidnativeapp.data.model.response.ComicResourceType
 
 data class ObjectCredit(
-    @SerializedName("id") val id: Int?,
-    @SerializedName("name") val name: String?
-)
+    @SerializedName("id") override val id: Int?,
+    @SerializedName("name") override val name: String?,
+    @SerializedName("api_detail_url") val apiDetailUrl: String?, // URL to the issue detail
+    @SerializedName("site_detail_url") val siteDetailUrl: String?,
+    override val image: Image?,
+) : ComicResource {
+    override val apiId get(): String? = apiDetailUrl?.split("/")?.dropLast(1)?.lastOrNull()
+    override val resourceType get(): ComicResourceType = ComicResourceType.OBJECT
+}
--- app/src/main/java/s/m/mota/comicvineandroidnativeapp/data/model/StoryArcCredits.kt
+++ app/src/main/java/s/m/mota/comicvineandroidnativeapp/data/model/StoryArcCredits.kt
@@ -3,7 +3,7 @@ package s.m.mota.comicvineandroidnativeapp.data.model
 import com.google.gson.annotations.SerializedName
 import s.m.mota.comicvineandroidnativeapp.data.model.response.ComicResourceType
 
-data class LocationCredit(
+data class StoryArcCredits(
     @SerializedName("id") override val id: Int?,
     @SerializedName("name") override val name: String?,
     @SerializedName("api_detail_url") val apiDetailUrl: String?, // URL to the issue detail
@@ -11,5 +11,6 @@ data class LocationCredit(
     override val image: Image?,
 ) : ComicResource {
     override val apiId get(): String? = apiDetailUrl?.split("/")?.dropLast(1)?.lastOrNull()
-    override val resourceType get(): ComicResourceType = ComicResourceType.LOCATION
+    override val resourceType get() : ComicResourceType = ComicResourceType.PERSON
 }
+
