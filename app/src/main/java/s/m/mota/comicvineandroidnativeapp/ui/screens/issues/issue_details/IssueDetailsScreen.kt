9cc0304717a32b495ceb711c5e47e0a0d8e3f3f7
Samuel Mota <samuelmmota@gmail.com>
2025-01-10 15:53:52 +0000

    Implement DisplayComicHtmlAnnotatedElement in parsedDescription on Characters, Issue and Volume Details

--- app/src/main/java/s/m/mota/comicvineandroidnativeapp/ui/screens/issues/issue_details/IssueDetailsScreen.kt
+++ app/src/main/java/s/m/mota/comicvineandroidnativeapp/ui/screens/issues/issue_details/IssueDetailsScreen.kt
@@ -3,6 +3,7 @@ package s.m.mota.comicvineandroidnativeapp.ui.screens.issues.issue_details
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -11,6 +12,7 @@ import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
@@ -21,6 +23,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -29,7 +33,7 @@ import androidx.navigation.NavController
 import kotlinx.coroutines.launch
 import s.m.mota.comicvineandroidnativeapp.R
 import s.m.mota.comicvineandroidnativeapp.ui.component.CircularIndeterminateProgressBar
-import s.m.mota.comicvineandroidnativeapp.ui.component.text.DisplayAnnotatedHtmlContent
+import s.m.mota.comicvineandroidnativeapp.ui.component.text.DisplayComicHtmlAnnotatedElement
 import s.m.mota.comicvineandroidnativeapp.ui.theme.FloatingActionBackground
 
 @Composable
@@ -85,12 +89,32 @@ fun IssueDetailsScreen(navController: NavController, issueApiId: String) {
                         IssueDetailsView(it, navController)
                     }
                 }
-                item {
-                    parsedDescription?.let {
-                        DisplayAnnotatedHtmlContent(
-                            title = stringResource(R.string.description),
-                            annotatedElements = it,
-                            navController = navController)
+
+                parsedDescription?.let { descriptionElements ->
+                    item {
+                        Text(
+                            text = stringResource(R.string.description),
+                            style = MaterialTheme.typography.titleLarge.copy(
+                                color = MaterialTheme.colorScheme.primary,
+                                fontWeight = FontWeight.Bold
+                            ),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 8.dp),
+                            textAlign = TextAlign.Center
+                        )
+
+                    }
+
+                    descriptionElements.forEach { element ->
+                        item {
+                            DisplayComicHtmlAnnotatedElement(
+                                annotatedElement = element,
+                                navController = navController,
+                                modifier = Modifier
+                                    .padding(vertical = 4.dp, horizontal = 8.dp)
+                            )
+                        }
                     }
                 }
             }
